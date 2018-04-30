cf4bdc02a2450ab706a63a7765765b9fe43d869b
davidgong89 <gongtuo@gmail.com>
2018-04-30 14:22:24 +0800

    js update

--- js/index.js
+++ js/index.js
@@ -1,5 +1,47 @@
 document.addEventListener('deviceready', this.onDeviceReady, false);
 
+$('document').ready(function(){
+	$("input[type=submit]").click(function(e) {
+		
+		//clear out variables
+		var portal_code;
+		var code_chap_1;
+		var code_chap_2;
+		
+		
+		var portal_code = $("#code").val();
+		var code_chap_1 = $("#code-chap-1").val();
+		var code_chap_2 = $("#code-chap-2").val();
+		
+		
+		//if you have 2 forms in one html file, pls give different id name for the user input if not got issue
+		if (portal_code == 'tzuyu14061999') {
+			e.preventDefault();
+			window.location.href = "chapter2.html";
+			
+		}else if(portal_code == 'xxx0xxx'){
+			//chapter 3 link
+			e.preventDefault();
+			window.location.href = "chapter3.html";
+			
+		}else if (code_chap_1 == 'tzuyu14061999') {
+			//chapter 1 validation
+			e.preventDefault();
+			window.location.href = "chapter2.html";
+			
+		}else if (code_chap_2 == 'xxx0xxx') {
+			//chapter 2 validation
+			e.preventDefault();
+			window.location.href = "chapter3.html";
+			
+		}else{
+			e.preventDefault();
+			window.location.href = "index.html#portal";
+		}
+	});
+
+});
+
 function onDeviceReady(){
     console.log('Device is Ready');
     
@@ -19,44 +61,3 @@ function callMeToShowBanners() {
 
 
 
-$('document').ready(function(){
-	$("input[type=submit]").click(function(e) {
-		
-		//clear out variables
-		var portal_code;
-		var code_chap_1;
-		var code_chap_2;
-		
-		
-		var portal_code = $("#code").val();
-		var code_chap_1 = $("#code-chap-1").val();
-		var code_chap_2 = $("#code-chap-2").val();
-		
-		
-		//if you have 2 forms in one html file, pls give different id name for the user input if not got issue
-		if (portal_code === 'tzuyu14061999') {
-			e.preventDefault();
-			window.location.href = "chapter2.html";
-			
-		}else if(portal_code === 'xxx0xxx'){
-			//chapter 3 link
-			e.preventDefault();
-			window.location.href = "chapter3.html";
-			
-		}else if (code_chap_1 === 'tzuyu14061999') {
-			//chapter 1 validation
-			e.preventDefault();
-			window.location.href = "chapter2.html";
-			
-		}else if (code_chap_2 === 'xxx0xxx') {
-			//chapter 2 validation
-			e.preventDefault();
-			window.location.href = "chapter3.html";
-			
-		}else{
-			e.preventDefault();
-			window.location.href = "index.html#portal";
-		}
-	});
-
-});
