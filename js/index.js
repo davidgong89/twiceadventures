d21af01b249209e31e97e609b41695ec947e8e49
davidgong89 <gongtuo@gmail.com>
2018-04-30 13:49:24 +0800

    updates

--- js/index.js
+++ js/index.js
@@ -2,34 +2,61 @@ document.addEventListener('deviceready', this.onDeviceReady, false);
 
 function onDeviceReady(){
     console.log('Device is Ready');
+    
+    // Set AdMobAds options:
+    admob.setOptions({
+        publisherId:          "ca-app-pub-2624201443777881~7294073535",  // Required
+    });
 }
+
+admob.createBannerView({
+    autoShowBanner: false
+});
+
+function callMeToShowBanners() {
+    admob.showBannerAd(true);
+}
+
+
 
 $('document').ready(function(){
 	$("input[type=submit]").click(function(e) {
+		
+		//clear out variables
+		var portal_code;
+		var code_chap_1;
+		var code_chap_2;
+		
+		
 		var portal_code = $("#code").val();
 		var code_chap_1 = $("#code-chap-1").val();
+		var code_chap_2 = $("#code-chap-2").val();
+		
 		
 		//if you have 2 forms in one html file, pls give different id name for the user input if not got issue
-		//if you have 2 forms in one html file, pls give different id name for the user input if not got issue
-		if (portal_code == 'tzuyu14061999') {
+		if (portal_code === 'tzuyu14061999') {
 			e.preventDefault();
 			window.location.href = "chapter2.html";
+			
+		}else if(portal_code === 'xxx0xxx'){
+			//chapter 3 link
+			e.preventDefault();
+			window.location.href = "chapter3.html";
+			
+		}else if (code_chap_1 === 'tzuyu14061999') {
+			//chapter 1 validation
+			e.preventDefault();
+			window.location.href = "chapter2.html";
+			
+		}else if (code_chap_2 === 'xxx0xxx') {
+			//chapter 2 validation
+			e.preventDefault();
+			window.location.href = "chapter3.html";
+			
 		}else{
 			e.preventDefault();
 			window.location.href = "index.html#portal";
 		}
-		
-		
-		
-		if (code_chap_1 == 'tzuyu14061999') {
-			e.preventDefault();
-			window.location.href = "chapter2.html";
-		}else{
-			e.preventDefault();
-			window.location.href = "index.html#portal";
-		}
-		
-		
 	});
 
 });
