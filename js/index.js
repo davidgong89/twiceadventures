0e34a65af4dbb1671b69cc98661501832c1a0207
davidgong89 <gongtuo@gmail.com>
2018-04-30 14:48:25 +0800

    update to JS

--- js/index.js
+++ js/index.js
@@ -6,8 +6,9 @@ function onDeviceReady(){
     // Set AdMobAds options: 
     admob.setOptions({
       publisherId:          "ca-app-pub-3940256099942544/6300978111",  // Required 
+      interstitialAdId:     "ca-app-pub-3940256099942544/1033173712",  // interstitial Ads
     });
-    
+  
     admob.createBannerView();
 }
 
